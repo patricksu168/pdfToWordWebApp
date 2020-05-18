ade89e7f62b400307eeef34fbee6df52705db5eb
patricksu168 <0ocookiezio0@gmail.com>
2020-05-18 17:08:06 +1000

    Updates

---
+++ precache-manifest.c0e75bd385db5eb99fb94394e5ef408c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb0ef93a6efd650ab13d8bd0c5a3c05",
+    "url": "/PdfToWordWebApp/index.html"
+  },
+  {
+    "revision": "c6d4beaf46dae181eedc",
+    "url": "/PdfToWordWebApp/static/css/main.cdba86e2.chunk.css"
+  },
+  {
+    "revision": "8fe5e224fa5c55b6ab75",
+    "url": "/PdfToWordWebApp/static/js/2.5ac3db64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PdfToWordWebApp/static/js/2.5ac3db64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d4beaf46dae181eedc",
+    "url": "/PdfToWordWebApp/static/js/main.5d843f01.chunk.js"
+  },
+  {
+    "revision": "4e9678a1739aa3d4d131",
+    "url": "/PdfToWordWebApp/static/js/runtime-main.3b204bcf.js"
+  }
+]);
